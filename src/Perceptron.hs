8c3e8a1bfb46f0842279f898fcb9deb54872b5d9
Valdson Francisco Silva Santos Júnior <by_valdson@live.com>
2022-07-27 00:36:26 -0300

    Configuração de testes e alteração nas assinaturas

--- src/Perceptron.hs
+++ src/Perceptron.hs
@@ -1,23 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
 module Perceptron where
 
 newtype Perceptron = Perceptron { pesos :: [Double] }
     deriving (Eq, Show)
 
+data ConfiguracaoTreinamento = Config {
+    nrDeEpocas :: Word,
+    taxaDeAprendizado :: Double
+}
+
 perceptron :: [Double] -> Perceptron
 perceptron ps = Perceptron (-1 : ps)
 
-classificar :: [Double] -> Perceptron -> Double
-classificar amostra = ativacao . sum . zipWith (*) (1 : amostra) . pesos
+classificar :: Perceptron -> [Double] -> Double
+classificar p amostra = ativacao . sum . zipWith (*) (1 : amostra) $ pesos p
     where ativacao n = if n >= 0 then 1 else 0
 
-treinar :: [([Double], Double)] -> Word -> Double -> Perceptron -> Perceptron
-treinar amostras nrEpocas taxaDeAprendizado p
-    | nrEpocas == 0 || p == novoP = p
-    | otherwise = treinar amostras (nrEpocas - 1) taxaDeAprendizado novoP
+treinar :: ConfiguracaoTreinamento -- ^ Configuração de treinamento do perceptron
+  -> [([Double], Double)] -- ^ Lista de amostras e resultados esperados
+  -> Perceptron -- ^ Rede perceptron não treinada
+  -> Perceptron
+treinar Config{..} amostras p
+    | nrDeEpocas == 0 || p == novoP = p
+    | otherwise = treinar config amostras novoP
     where
+    config = Config (nrDeEpocas - 1) taxaDeAprendizado
     novoP = foldr atualizar p amostras
     atualizar (amostra, resultadoEsperado) rede =
-        let resultadoObtido = classificar amostra rede
+        let resultadoObtido = classificar rede amostra
             erro = resultadoEsperado - resultadoObtido
         in
         Perceptron $
--- test/Main.hs
+++ test/Main.hs
@@ -1,79 +1,75 @@
 module Main where
 
 import Perceptron
-    ( Perceptron, classificar, treinar, perceptron)
--- import System.Directory ( getCurrentDirectory, getDirectoryContents )
--- import System.FilePath ( (</>) )
+    ( ConfiguracaoTreinamento (..)
+    , Perceptron
+    , classificar
+    , perceptron
+    , treinar
+    )
+import Test.Hspec ( describe, hspec, it, shouldBe )
 
-p :: Perceptron
-p = perceptron [0, 0, 0]
+configuracao :: ConfiguracaoTreinamento
+configuracao = Config {
+        nrDeEpocas = 100,
+        taxaDeAprendizado = 0.01
+    }
 
-printAND :: IO ()
-printAND = do
-    let conjuntoDeTreino =
-            [ ([1, 1, 1], 1)
-            , ([1, 1, 0], 0)
-            , ([1, 0, 1], 0)
-            , ([0, 1, 1], 0)
-            ]
+treinarPerceptronAnd :: Perceptron -> Perceptron
+treinarPerceptronAnd = treinar configuracao
+    [ ([1, 1, 1], 1)
+    , ([0, 1, 0], 0)
+    , ([1, 0, 0], 0)
+    , ([0, 1, 1], 0)
+    ]
 
-    let amostras =
-            [ [1, 0, 0]
-            , [0, 1, 0]
-            , [0, 0, 1]
-            , [0, 0, 0]
-            , [1, 1, 0]
-            , [1, 0, 1]
-            , [0, 1, 1]
-            , [1, 1, 1]
-            ]
-
-    let taxaDeAprendizado = 0.1
-    let redeTreinada = treinar conjuntoDeTreino 100 taxaDeAprendizado p
-    print redeTreinada
-    mapM_ (showAmostra redeTreinada) amostras
-
-showAmostra :: Perceptron -> [Double] -> IO ()
-showAmostra rede amostra = do
-    let resultado = classificar amostra rede
-    putStrLn $ "Entrada: " <> show amostra <> " => " <> show resultado
-
-
-printOR :: IO ()
-printOR = do
-    let conjuntoDeTreino =
-            [ ([0, 0, 1], 1)
-            , ([0, 1, 0], 1)
-            , ([1, 0, 0], 1)
-            , ([0, 0, 0], 0)
-            ]
-
-    let amostras =
-            [ [ 1, 0, 0]
-            , [ 0, 1, 0]
-            , [ 0, 0, 1]
-            , [ 0, 0, 0]
-            , [ 1, 1, 0]
-            , [ 1, 0, 1]
-            , [ 0, 1, 1]
-            , [ 1, 1, 1]
-            ]
-
-    let taxaDeAprendizado = 0.1
-    let redeTreinada = treinar conjuntoDeTreino 100 taxaDeAprendizado p
-    print redeTreinada
-    mapM_ (showAmostra redeTreinada) amostras
-
--- printCHARS :: IO ()
--- printCHARS = do
---     dir <- getCurrentDirectory
---     let trainingSET = dir </> "treino"
---     let testingSET = dir </> "teste"
-
---     getDirectoryContents trainingSET >>= print
---     getDirectoryContents testingSET >>= print
---     print dir
+treinarPerceptronOr :: Perceptron -> Perceptron
+treinarPerceptronOr = treinar configuracao
+    [ ([0, 0, 1], 1)
+    , ([0, 1, 0], 1)
+    , ([1, 0, 0], 1)
+    , ([0, 0, 0], 0)
+    ]
 
 main :: IO ()
-main = printAND
-    >> printOR
+main = do
+    let p = perceptron [0, 0, 0]
+    let perceptronOr = classificar $ treinarPerceptronOr p
+    let perceptronAnd = classificar $ treinarPerceptronAnd p
+
+    hspec $ do
+        describe "Perceptron - OR" $ do
+            it "p([0, 0, 0]) == 0" $
+                perceptronOr [0,0,0] `shouldBe` 0
+            it "p([0, 0, 1]) == 1" $
+                perceptronOr [0,0,1] `shouldBe` 1
+            it "p([0, 1, 0]) == 1" $
+                perceptronOr [0,1,0] `shouldBe` 1
+            it "p([1, 0, 0]) == 1" $
+                perceptronOr [1,0,0] `shouldBe` 1
+            it "p([1, 0, 1]) == 1" $
+                perceptronOr [1,0,1] `shouldBe` 1
+            it "p([0, 1, 1]) == 1" $
+                perceptronOr [0,1,1] `shouldBe` 1
+            it "p([1, 1, 0]) == 1" $
+                perceptronOr [1,1,0] `shouldBe` 1
+            it "p([1, 1, 1]) == 1" $
+                perceptronOr [1,1,1] `shouldBe` 1
+
+        describe "Perceptron - AND" $ do
+            it "p([0, 0, 0]) == 0" $
+                perceptronAnd [0,0,0] `shouldBe` 0
+            it "p([0, 0, 1]) == 0" $
+                perceptronAnd [0,0,1] `shouldBe` 0
+            it "p([0, 1, 0]) == 0" $
+                perceptronAnd [0,1,0] `shouldBe` 0
+            it "p([1, 0, 0]) == 0" $
+                perceptronAnd [1,0,0] `shouldBe` 0
+            it "p([1, 0, 1]) == 0" $
+                perceptronAnd [1,0,1] `shouldBe` 0
+            it "p([0, 1, 1]) == 0" $
+                perceptronAnd [0,1,1] `shouldBe` 0
+            it "p([1, 1, 0]) == 0" $
+                perceptronAnd [1,1,0] `shouldBe` 0
+            it "p([1, 1, 1]) == 1" $
+                perceptronAnd [1,1,1] `shouldBe` 1
